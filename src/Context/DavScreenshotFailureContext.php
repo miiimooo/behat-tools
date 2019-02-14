b14b18742d1a22e53c8849ed7ba2cc4f9225570a
Michael Moritz <miiimooo@gmail.com>
2019-02-14 15:56:44 +0100

    Mikkes additions

--- src/Context/DavScreenshotFailureContext.php
+++ src/Context/DavScreenshotFailureContext.php
@@ -79,7 +79,7 @@ class DavScreenshotFailureContext implements Context {
     }
     $basename .= '-' . date('Ymd_Hi');
     if (!function_exists('file_prepare_directory')) {
-      printf('*** not running inside a Drupal environment is not fully supported. Ensure the local screenhot target folder exists yourself ***');
+      printf('*** not running inside a Drupal environment is not fully supported. Ensure the local screenshot target folder exists yourself ***');
     }
     else {
       file_prepare_directory($this->failurePath, FILE_CREATE_DIRECTORY);
@@ -98,22 +98,38 @@ class DavScreenshotFailureContext implements Context {
     sprintf("HTML dump available at: %s\n", $filename);
   }
 
+  /**
+   * Stores screenshots on a webdav server
+   * Requires environment variables:
+   * WEBDAV_HOST, WEBDAV_USERNAME, WEBDAV_PASSWORD.
+   *
+   * @param $path
+   * @param \Behat\Behat\Hook\Scope\StepScope $scope
+   *
+   * @throws \Sabre\DAV\ClientException
+   */
   public function storeDumps($path, StepScope $scope) {
-    $screenshots_folder = trim(getenv('WEBDAV_FOLDER') ?: 'screenshots', "/");
     $settings = [
-      'baseUri' => getenv('WEBDAV_HOST') . '/' . $screenshots_folder . '/',
+      'baseUri' => getenv('WEBDAV_HOST') . '/screenshots/',
       'userName' => getenv('WEBDAV_USERNAME'),
       'password' => getenv('WEBDAV_PASSWORD'),
     ];
-    $prefix = array_filter(['DRONE_REPO_NAME', 'DRONE_BUILD_NUMBER'], function($v) {
+    $prefix = array_filter(['DRONE_REPO_OWNER', 'DRONE_REPO_NAME', 'DRONE_BUILD_NUMBER'], function ($v) {
       return getenv($v);
     });
-    $prefix = implode('-', array_map('getenv', $prefix));
+    $prefix = implode('/', array_map('getenv', $prefix));
 
     $client = new Client($settings);
+
+    // Create subfolders.
+    $createrepo_owner = $client->request('MKCOL', '/screenshots/' . getenv('DRONE_REPO_OWNER'));
+    $create_repo_name = $client->request('MKCOL', '/screenshots/' . getenv('DRONE_REPO_OWNER') . '/' . getenv('DRONE_REPO_NAME'));
+    $create_repo_build = $client->request('MKCOL', '/screenshots/' . getenv('DRONE_REPO_OWNER') . '/' . getenv('DRONE_REPO_NAME') . '/' . getenv('DRONE_BUILD_NUMBER' ));
+
+
     foreach (['.png', '.html'] as $extension) {
       if (file_exists($path . $extension)) {
-        $filename = "$prefix" . "-" . basename($path . $extension);
+        $filename = "$prefix/" . basename($path . $extension);
         $response = $client->request('PUT', $filename, file_get_contents($path . $extension));
         if ($response['statusCode'] != 201) {
           var_dump($response);
@@ -123,5 +139,6 @@ class DavScreenshotFailureContext implements Context {
         }
       }
     }
+
   }
 }
